48bee416fd94b2988408c890ae339f039518eab6
BlazeCodeDev <blazecodeapps@gmail.com>
2021-12-23 17:46:40 +0100

    app now opens onClick of the notification

--- app/src/main/java/com/blazecode/tsviewer/util/notification/NotificationManager.kt
+++ app/src/main/java/com/blazecode/tsviewer/util/notification/NotificationManager.kt
@@ -2,20 +2,28 @@ package com.blazecode.tsviewer.util.notification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.blazecode.tsviewer.MainActivity
 import com.blazecode.tsviewer.R
 
 class NotificationManager(private val context: Context) {
 
     fun post(clientListNames: MutableList<String>){
+
+        val intent = Intent(context, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        val clickIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
         if (clientListNames.size > 0) {
             val builder = NotificationCompat.Builder(context, context.getString(R.string.notificationChannelClientID))
                 .setSmallIcon(R.drawable.ic_notification_icon)
                 .setContentText(clientListNames.joinToString())
                 .setPriority(NotificationCompat.PRIORITY_MIN)
+                .setContentIntent(clickIntent)
 
             if (clientListNames.size == 1) builder.setContentTitle("${clientListNames.size} ${context.resources.getString(R.string.client_connected)}")
             else builder.setContentTitle("${clientListNames.size} ${context.resources.getString(R.string.clients_connected)}")
