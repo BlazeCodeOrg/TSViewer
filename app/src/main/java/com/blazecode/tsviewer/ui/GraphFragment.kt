0e1e3ccef507deb0217808fef05ca8090dbadd53
BlazeCodeDev <blazecodeapps@gmail.com>
2022-04-08 19:31:30 +0200

    Datapoints are now showing

--- app/src/main/java/com/blazecode/tsviewer/ui/GraphFragment.kt
+++ app/src/main/java/com/blazecode/tsviewer/ui/GraphFragment.kt
@@ -1,14 +1,14 @@
 package com.blazecode.tsviewer.ui
 
+import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.blazecode.tsviewer.R
-import com.blazecode.tsviewer.databinding.ActivityMainBinding
 import com.blazecode.tsviewer.databinding.FragmentGraphBinding
-import com.blazecode.tsviewer.databinding.MainFragmentBinding
 import com.blazecode.tsviewer.util.database.UserCount
 import com.blazecode.tsviewer.util.database.UserCountDAO
 import com.blazecode.tsviewer.util.database.UserCountDatabase
@@ -16,10 +16,11 @@ import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.data.Entry
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
+
 
 class GraphFragment(override val coroutineContext: CoroutineContext) : Fragment(), CoroutineScope {
 
@@ -29,27 +30,12 @@ class GraphFragment(override val coroutineContext: CoroutineContext) : Fragment(
     lateinit var db: UserCountDatabase
     lateinit var userCountDAO: UserCountDAO
 
-    lateinit var barData : BarData
-
-    var data = mutableListOf<UserCount>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // OPEN DB
         db = UserCountDatabase.build(requireContext())
         userCountDAO = db.userCountDao()
-
-        launch {
-            data = userCountDAO.getAll()
-        }
-
-        var entries: MutableList<BarEntry> = mutableListOf()
-        for(UserCount in data){
-            entries.add(BarEntry(UserCount.timestamp!!.toFloat(), UserCount.amount!!.toFloat(), UserCount.names))
-        }
-        val barChartDataset = BarDataSet(entries, "Clients")
-        barChartDataset.color = R.color.primary
-
-        barData = BarData(barChartDataset)
     }
 
     override fun onCreateView(
@@ -63,6 +49,54 @@ class GraphFragment(override val coroutineContext: CoroutineContext) : Fragment(
         super.onViewCreated(view, savedInstanceState)
 
         val graph: BarChart = view.findViewById(R.id.chart)
-        graph.data = barData
+        getDataFromDataBase(graph)
+    }
+
+    private fun getDataFromDataBase(barChart: BarChart){
+        var dataTemp = mutableListOf<UserCount>()
+        launch(Dispatchers.IO) {
+            dataTemp = userCountDAO.getAll()
+            remapData(dataTemp, barChart)
+        }
+    }
+
+    private fun remapData(data: MutableList<UserCount>, barChart: BarChart){
+        // CONVERT USERCOUNT TO ENTRIES
+        var entries: MutableList<BarEntry> = mutableListOf()
+        for(UserCount in data){
+            entries.add(BarEntry(UserCount.timestamp!!.toFloat(), UserCount.amount!!.toFloat(), UserCount.names))
+        }
+
+        // STYLE DATASET
+        val barChartDataset = BarDataSet(entries, "Clients")
+        barChartDataset.color = R.color.text
+
+        val barData = BarData(barChartDataset)
+        styleGraph(barData, barChart)
+    }
+
+    private fun styleGraph(barData: BarData, barChart: BarChart){
+        // SET GRAPH HEIGHT TO HALF OF THE SCREEN
+        val display = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requireActivity().display
+        } else {
+            requireActivity().windowManager.defaultDisplay
+        }
+        val size = Point()
+        display!!.getSize(size)
+        val params: ViewGroup.LayoutParams = barChart.layoutParams
+        params.height = size.y / 2
+        barChart.layoutParams = params
+
+        // STYLE GRAPH
+        barChart.setBackgroundColor(resources.getColor(R.color.background))
+        barChart.setDrawBorders(false)
+        barChart.description.text = ""
+
+        assignDataToGraph(barData, barChart)
+    }
+
+    private fun assignDataToGraph(barData: BarData, barChart: BarChart){
+        barChart.data = barData
     }
 }
