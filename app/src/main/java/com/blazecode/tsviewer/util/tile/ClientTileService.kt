fa580ac097d7424f6ee88b6943eb58931ce58fd8
BlazeCodeDev <blazecodeapps@gmail.com>
2022-05-26 17:27:14 +0200

    Add error message >A10, Fix Crash

--- app/src/main/java/com/blazecode/tsviewer/util/tile/ClientTileService.kt
+++ app/src/main/java/com/blazecode/tsviewer/util/tile/ClientTileService.kt
@@ -4,9 +4,10 @@ import android.content.Intent
 import android.os.Build
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
-import androidx.annotation.RequiresApi
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.blazecode.tsviewer.MainActivity
+import com.blazecode.tsviewer.R
 
 
 class ClientTileService : TileService() {
@@ -14,10 +15,13 @@ class ClientTileService : TileService() {
     override fun onClick() {
         super.onClick()
 
-        startActivityAndCollapse(Intent(applicationContext, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startActivityAndCollapse(Intent(applicationContext, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        } else {
+            Toast.makeText(this, resources.getString(R.string.notSupportedBelowAndroidTen), Toast.LENGTH_LONG).show()
+        }
     }
 
-    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onStartListening() {
         super.onStartListening()
 
@@ -30,7 +34,9 @@ class ClientTileService : TileService() {
         else qsTile.state = Tile.STATE_INACTIVE
 
         //SET SUBTITLE
-        qsTile.subtitle = tileSetting.getString("subtitle", "")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            qsTile.subtitle = tileSetting.getString("subtitle", "")
+        }
 
         //APPLY
         qsTile.updateTile()
