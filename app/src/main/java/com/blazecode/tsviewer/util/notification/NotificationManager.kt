b9b3c803fb76b7f9d75736692dde2a0118fab1b7
BlazeCodeDev <blazecodeapps@gmail.com>
2022-05-30 18:58:40 +0200

    Remove obsolete code

--- app/src/main/java/com/blazecode/tsviewer/util/notification/NotificationManager.kt
+++ app/src/main/java/com/blazecode/tsviewer/util/notification/NotificationManager.kt
@@ -5,7 +5,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.blazecode.tsviewer.MainActivity
@@ -37,14 +36,12 @@ class NotificationManager(private val context: Context) {
     }
 
     fun createChannel(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = context.getString(R.string.notificationChannelClient)
-            val importance = NotificationManager.IMPORTANCE_MIN
-            val channel = NotificationChannel(context.getString(R.string.notificationChannelClientID), name, importance).apply{}
-            // Register the channel with the system
-            val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
+        val name = context.getString(R.string.notificationChannelClient)
+        val importance = NotificationManager.IMPORTANCE_MIN
+        val channel = NotificationChannel(context.getString(R.string.notificationChannelClientID), name, importance).apply{}
+        // Register the channel with the system
+        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 
     fun removeNotification(){
