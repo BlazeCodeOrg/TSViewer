cb718cd959305205a3552dfbfedf342b425cc0c8
BlazeCodeDev <blazecodeapps@gmail.com>
2023-03-07 18:19:13 +0100

    Implement spacer recognition

--- app/src/main/java/com/blazecode/tsviewer/views/TsChannelList.kt
+++ app/src/main/java/com/blazecode/tsviewer/views/TsChannelList.kt
@@ -6,12 +6,15 @@
 
 package com.blazecode.tsviewer.views
 
+import android.graphics.Paint
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,8 +46,23 @@ fun TsChannelList(
 private fun ChannelView(
     title: String
 ){
-    Card(modifier = Modifier.fillMaxWidth()) {
-        Text(text = title)
+    if (isSpacer(title)){
+        val regex = Regex(".\$")
+        val char = regex.find(title)!!.value
+        val charWidth = Paint().measureText(char)
+
+        BoxWithConstraints(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+            val n = (maxWidth / charWidth).value.toInt() / 6 * 4
+            Text(text = buildString {
+                for (i in 1..n) {
+                    append(char)
+                }
+            })
+        }
+    } else {
+        Card(modifier = Modifier.fillMaxWidth()) {
+            Text(text = title)
+        }
     }
 }
 
@@ -57,11 +75,18 @@ private fun MemberView(
     }
 }
 
+private fun isSpacer(name: String): Boolean {
+    val pattern = Regex("^\\[\\*spacer\\d\\]")
+    val result = pattern.containsMatchIn(name)
+    return result
+}
+
 @Preview
 @Composable
 private fun Preview(){
     val demoList = mutableListOf<TsChannel>(
         TsChannel(name = "Channel 1", members = mutableListOf(TsClient(nickname = "Member 1"), TsClient(nickname = "Member 2"))),
-    )
+        TsChannel(name = "[*spacer1]_"),
+        )
     TsChannelList(channels = demoList)
 }
