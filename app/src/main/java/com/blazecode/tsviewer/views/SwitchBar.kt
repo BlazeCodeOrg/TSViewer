8324d7dbfc595b26a74c750f35a98317dfe18e60
BlazeCodeDev <blazecodeapps@gmail.com>
2023-05-03 11:41:01 +0200

    Add summary

--- app/src/main/java/com/blazecode/tsviewer/views/SwitchBar.kt
+++ app/src/main/java/com/blazecode/tsviewer/views/SwitchBar.kt
@@ -6,11 +6,21 @@
 
 package com.blazecode.eventtool.views
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,6 +31,7 @@ import com.blazecode.tsviewer.R
 @Composable
 fun SwitchBar(
     title: String,
+    summary: String = "",
     checked: Boolean,
     onCheckChanged: (Boolean) -> Unit){
     Card (modifier = Modifier.padding(dimensionResource(R.dimen.medium_padding), dimensionResource(R.dimen.small_padding), dimensionResource(R.dimen.medium_padding), 0.dp),
@@ -28,6 +39,7 @@ fun SwitchBar(
         Row (modifier = Modifier.fillMaxWidth().padding(dimensionResource(R.dimen.medium_padding)), verticalAlignment = Alignment.CenterVertically){
             Column (modifier = Modifier.weight(5f, true)){
                 Text(title, color = getTextColor(checked), fontSize = 18.sp)
+                if(summary.isNotEmpty()) Text(summary, color = getTextColor(checked), fontSize = 16.sp, modifier = Modifier.alpha(.7f))
             }
             Box (modifier = Modifier.fillMaxWidth().padding(0.dp, 0.dp, dimensionResource(R.dimen.medium_padding), 0.dp).weight(1.5f), contentAlignment = Alignment.CenterEnd){
                 Switch(
@@ -59,7 +71,7 @@ private fun getTextColor(checked: Boolean): Color {
 @Composable
 private fun Preview(){
     Column{
-        SwitchBar(title = "title", checked = true){}
+        SwitchBar(title = "title", checked = true, summary = "summary"){}
         SwitchBar(title = "title", checked = false){}
     }
 }
