884e110fe2c130985b692ac94b1d98985a749e15
Ralf Lehmann <blazecodeapps@gmail.com>
2023-07-04 16:46:22 +0200

    Add wear refresh_request

--- app/src/core/java/wear/WearableListenerService.kt
+++ app/src/core/java/wear/WearableListenerService.kt
@@ -6,15 +6,22 @@
 
 package wear
 
+import android.content.Context
 import android.content.Intent
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
 import com.blazecode.tsviewer.MainActivity
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
+import util.ClientsWorker
 
 class WearableListenerService: WearableListenerService() {
 
     companion object {
-        const val LAUNCH_PATH = "/start-activity"
+        private const val LAUNCH_PATH = "/start-activity"
+        private const val REQUEST_REFRESH = "/request-refresh"
     }
 
     override fun onMessageReceived(messageEvent: MessageEvent) {
@@ -22,11 +29,32 @@ class WearableListenerService: WearableListenerService() {
 
         when (messageEvent.path) {
             LAUNCH_PATH -> {
-                this@WearableListenerService.startActivity(
-                    Intent(this@WearableListenerService, MainActivity::class.java)
-                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                )
+                startActivity(this@WearableListenerService)
+            }
+            REQUEST_REFRESH -> {
+                refreshRequest(this@WearableListenerService)
             }
         }
+    }
+
+    private fun startActivity(context: Context) {
+        context.startActivity(
+            Intent(context, MainActivity::class.java)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        )
+    }
+
+    private fun refreshRequest(context: Context){
+        var workManager: WorkManager = context.let { WorkManager.getInstance(context) }
+
+        val data = Data.Builder()
+        data.putBoolean("suppress_db", true)
+        data.putBoolean("suppress_notification", true)
+
+        val oneTimeclientWorkRequest: WorkRequest = OneTimeWorkRequestBuilder<ClientsWorker>()
+            .setInputData(data.build())
+            .build()
+
+        workManager.enqueue(oneTimeclientWorkRequest)
     }
 }
