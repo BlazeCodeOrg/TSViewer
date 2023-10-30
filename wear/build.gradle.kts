94dd2cff14543456bb8d6cc3f4a2d3783b91e142
Ralf Lehmann <blazecodeapps@gmail.com>
2023-10-30 10:32:01 +0100

    Release V1.2

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.blazecode.tsviewer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         vectorDrawables {
             useSupportLibrary = true
         }
