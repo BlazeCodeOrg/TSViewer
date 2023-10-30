2605f4d29587f0162b6ce1f0222c0397ee24716a
Ralf Lehmann <blazecodeapps@gmail.com>
2023-10-30 13:01:24 +0100

    Disable minify & shrink because of unpredictable crashes

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -21,8 +21,8 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
